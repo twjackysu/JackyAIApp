872ab0dc460f9d474c1e6f9050da8c550e53aee3
Jacky Su <jacky19918@gmail.com>
2024-05-10 22:24:52 +0800

    chore: update eslint config

--- jackyaiapp.client/.eslintrc.cjs
+++ jackyaiapp.client/.eslintrc.cjs
@@ -14,7 +14,6 @@ module.exports = {
   plugins: ['react-refresh', 'import'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    'import/no-unresolved': 'error',
     'import/named': 'off',
     'import/namespace': 'off',
     'import/default': 'off',
@@ -29,20 +28,7 @@ module.exports = {
         alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
 
         // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
-
-        // use <root>/path/to/folder/tsconfig.json
-        project: 'path/to/folder',
-
-        // Multiple tsconfigs (Useful for monorepos)
-
-        // use a glob pattern
-        project: 'packages/*/tsconfig.json',
-
-        // use an array
-        project: ['packages/module-a/tsconfig.json', 'packages/module-b/tsconfig.json'],
-
-        // use an array of glob patterns
-        project: ['packages/*/tsconfig.json', 'other-packages/*/tsconfig.json'],
+        project: './tsconfig.json',
       },
     },
   },
